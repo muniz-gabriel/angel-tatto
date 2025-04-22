1b0d4bffc9d3a2a5f43ff4a096e58064f1ab2a92
muniz-gabriel <muniz.gabriel@aluno.ifsp.edu.br>
2025-04-22 17:46:21 -0300

    Criando Banner

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const dropdownToggle = document.querySelectorAll('[data-dropdown-toggle]');
+    dropdownToggle.forEach((toggle) => {
+        toggle.addEventListener('click', function() {
+            const parent = this.closest('.dropdown-item');
+            parent.classList.toggle('active');
+        })
+    })
+})
